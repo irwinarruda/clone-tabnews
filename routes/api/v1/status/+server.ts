7943a350fcbd8ad2e9588d7e9436b03d39fd5666
Irwin Arruda <arruda.irwin@gmail.com>
2023-12-17 14:42:35 -0300

    incrementa `/api/v1/status`

--- routes/api/v1/status/+server.ts
+++ routes/api/v1/status/+server.ts
@@ -2,8 +2,34 @@ import { json } from '@sveltejs/kit';
 import database from '~/infra/database';
 import type { RequestHandler } from './$types';
 
+export type GetStatusResponse = {
+	updated_at: string;
+	dependencies: {
+		database: {
+			version: string;
+			max_connections: number;
+			open_connections: number;
+		};
+	};
+};
+
 export const GET: RequestHandler = async function () {
-	const result = await database.query('SELECT 1 + 1;');
-	console.log('result', result.rows);
-	return json({ status: 'Alunos do curso.dev são pessoas acima da média' }, { status: 200 });
+	const updatedAt = new Date().toISOString();
+	const databaseData = await database.query(`
+    SHOW SERVER_VERSION;
+    SHOW MAX_CONNECTIONS;
+    SELECT COUNT(*)::int FROM PG_STAT_ACTIVITY WHERE datname = 'local_db';
+  `);
+	const respose: GetStatusResponse = {
+		updated_at: updatedAt,
+		dependencies: {
+			database: {
+				version: databaseData[0].rows[0].server_version,
+				max_connections: Number(databaseData[1].rows[0].max_connections),
+				open_connections: databaseData[2].rows[0].count
+			}
+		}
+	};
+
+	return json(respose, { status: 200 });
 };
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,4 +1,17 @@
 test('GET to /api/v1/status should return 200', async function () {
 	const response = await fetch('http://localhost:5173/api/v1/status');
 	expect(response.status).toBe(200);
+	const body = await response.json();
+	expect(body.updated_at).toBeDefined();
+	expect(body.updated_at).toBe(new Date(body.updated_at).toISOString());
+	expect(body.dependencies).toBeDefined();
+	const database = body.dependencies.database;
+	expect(database).toBeDefined();
+	expect(database.version).toBeDefined();
+	expect(Number(database.version.split('.')[0])).toBeGreaterThanOrEqual(0);
+	expect(database.max_connections).toBeDefined();
+	expect(database.max_connections).toBeGreaterThanOrEqual(0);
+	expect(database.open_connections).toBeDefined();
+	expect(database.open_connections).toBe(1);
+	expect(database.open_connections <= database.max_connections).toBeTruthy();
 });
