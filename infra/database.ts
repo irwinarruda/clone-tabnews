025791166ed6c380f3ce0f39df6f408ab3a85406
Irwin Arruda <arruda.irwin@gmail.com>
2023-12-21 00:34:53 -0300

    adiciona error handling e nova função de busca no `database.ts`

--- infra/database.ts
+++ infra/database.ts
@@ -1,6 +1,10 @@
 import { env } from '$env/dynamic/private';
 import * as db from 'pg';
 
+const sqlJoin = function <T extends string[] | ReadonlyArray<string>>(str: T) {
+	return str.reduce((acc, part, i) => acc + part + (i !== str.length - 1 ? `$${i + 1}` : ''), '');
+};
+
 const query = async function (queryObject: any, option1?: any) {
 	const client = new db.Client({
 		host: env.POSTGRES_HOST,
@@ -9,12 +13,33 @@ const query = async function (queryObject: any, option1?: any) {
 		database: env.POSTGRES_DB,
 		password: env.POSTGRES_PASSWORD
 	});
-	await client.connect();
-	const result = await client.query(queryObject, option1);
-	await client.end();
-	return result;
+	try {
+		await client.connect();
+		const result = await client.query(queryObject, option1);
+		return result;
+	} finally {
+		await client.end();
+	}
 } as db.ClientBase['query'];
 
+const sql = async function (literal: TemplateStringsArray, ...params: any) {
+	const client = new db.Client({
+		host: env.POSTGRES_HOST,
+		port: Number(env.POSTGRES_PORT),
+		user: env.POSTGRES_USER,
+		database: env.POSTGRES_DB,
+		password: env.POSTGRES_PASSWORD
+	});
+	try {
+		await client.connect();
+		const result = await client.query(sqlJoin(literal), params);
+		return result;
+	} finally {
+		await client.end();
+	}
+};
+
 export default {
-	query
+	query,
+	sql
 };
