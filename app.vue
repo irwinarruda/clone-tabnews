96373819848b4d33406990acf1cef881b7116dd0
irwinarruda <arruda.irwin@gmail.com>
2025-05-07 22:32:18 -0300

    chore: adds vitest as the test runner

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
 <template>
-  <div>
-    <h1>Forum de discussões sobre arquitetura de Front-end</h1>
-    <p>Compartilhe suas ideias e experiências!</p>
-    <p>Seja respeitoso(a)</p>
-    <p>
-      Pessoas que não conseguem conceber pontos de vista diferentes NÃO são bem
-      vindas.
-    </p>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Forum de discussões sobre arquitetura de Front-end</h1>
+    <p>Compartilhe suas ideias e experiências!</p>
+    <p>Seja respeitoso(a)</p>
+    <p>
+      Pessoas que não conseguem conceber pontos de vista diferentes NÃO são bem
+      vindas.
+    </p>
+  </div>
+</template>
