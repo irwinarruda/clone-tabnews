b11f6690b16bb7a2ea0c0312ccfe1eee382f5aff
irwinarruda <arruda.irwin@gmail.com>
2025-05-26 23:01:56 -0300

    feat: improves dev script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+import child from "child_process";
+
+process.stdout.write("\n🟨 Waiting for postgres");
+
+function check() {
+  function onDockerExec(_, stdout) {
+    if (stdout.search("accepting connections") !== -1) {
+      console.log("\n\n✅ Postgres is accepting connections");
+      return;
+    }
+    setTimeout(check, 350);
+    process.stdout.write(".");
+  }
+  child.exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    onDockerExec,
+  );
+}
+
+check();
