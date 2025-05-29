41b9a19483a4d3427cb9a107eb797785335b8d8c
irwinarruda <arruda.irwin@gmail.com>
2025-05-28 21:25:12 -0300

    chore(actions): adds lint action

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,14 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
   },
 });
-// Your custom configs here
