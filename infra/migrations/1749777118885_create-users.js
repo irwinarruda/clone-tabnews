7e41e470fe63a16d2f394f55b99e9ab37f03088f
irwinarruda <arruda.irwin@gmail.com>
2025-06-12 22:51:13 -0300

    feat: add user migration

---
+++ infra/migrations/1749777118885_create-users.js
@@ -0,0 +1,53 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      onUpdate: pgm.func("now()"),
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("users", {
+    cascade: true,
+  });
+};
